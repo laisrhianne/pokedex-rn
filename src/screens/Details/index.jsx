06e33f8bff506a4b3bf52c05fe43b27689aa7008
Laís Rhianne <lays.rhianne@gmail.com>
2021-11-20 16:12:45 -0300

    adição de imagem de pokemons

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
-import { Container, Title } from './styles.js';
+import api from '../../services/api';
+
+import { Container, Title, ImageContainer, Image } from './styles.js';
 
 export default function Details() {
-  return (
+  const [pokemonData, setPokemonData] = useState({});
+  const [loadingData, setLoadingData] = useState(false);
+
+  const route = useRoute();
+  const item = route.params && route.params.item;
+  const id = item && item.id;
+
+  async function getPokemonData() {
+    setLoadingData(true);
+    const response = await api.get(`pokemon/${id}`);
+    setPokemonData(response.data);
+    console.log(response.data.sprites);
+    setLoadingData(false);
+  }
+
+  useEffect(() => {
+    getPokemonData();
+  }, [route.params]);
+
+  return loadingData || !id ? (
+    <ActivityIndicator size='large' color='#FF0000' style={{ marginTop: 10 }} />
+  ) : (
     <Container>
-      <Title>Details</Title>
+      {pokemonData &&
+      pokemonData.sprites &&
+      pokemonData.sprites.front_default ? (
+        <ImageContainer>
+          <Image source={{ uri: pokemonData.sprites.front_default }} />
+          <Title>{pokemonData.name}</Title>
+          <Title>Peso: {pokemonData.weight}</Title>
+        </ImageContainer>
+      ) : null}
     </Container>
   );
 }
--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -2,4 +2,18 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View``;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 25px;
+`;
+
+export const ImageContainer = styled.View`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
