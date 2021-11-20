474af2021f4ef58083330bdde8b55aa0de85e926
Laís Rhianne <lays.rhianne@gmail.com>
2021-11-20 15:27:05 -0300

    uso da api para buscar os pokemons

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-
 import { TouchableOpacity } from 'react-native';
 
 import { Container, Title } from './styles.js';
 
+import api from '../../services/api';
+
 export default function Home() {
+  const [pokemons, setPokemons] = useState([]);
+
   const navigation = useNavigation();
+
+  async function getData() {
+    const response = await api.get('pokemon', { params: { limit: 150 } });
+    if (response.data && response.data.results) {
+      const pokemonData = response.data.results.map((data) => ({
+        ...data,
+        id: data.url.split('pokemon/')[1].replace('/', ''),
+      }));
+      setPokemons(pokemonData);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <Container>
